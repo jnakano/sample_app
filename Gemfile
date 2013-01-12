1c1070d66be95505d6ecd9c60c38a58063eea514
Jonhnson Nakano <jnakano89@gmail.com>
2013-01-12 14:37:08 -0800

    finished static pages

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jquery-rails', '2.0.2'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
-  gem 'rspec-rails'
+  gem 'rspec-rails', '2.11.0'
   gem 'guard-rspec', '1.2.1'
   gem 'guard-spork', '1.2.0'  
   gem 'spork', '0.9.2'
@@ -31,7 +31,7 @@ group :test do
   gem 'database_cleaner', '0.7.0'
   # gem 'launchy', '2.1.0'
   # gem 'rb-fsevent', '0.9.1', :require => false
-  # gem 'growl', '1.0.3'
+   gem 'growl', '1.0.3'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,76 @@
 require 'spec_helper'
 
-describe "Static Pages" do
+describe "Static pages" do
 
-	describe "home page" do
+	describe "Home page" do
 
-		it "should have the content 'Sample App' do"
+		it "should have the content 'Sample App'" do
 
 		visit '/static_pages/home'
-		page.should have_content('Sample App')
+		page.should have_selector('h1', :text=> 'Sample App')
+	end
+
+		it "should have the right title" do
+				visit '/static_pages/home'
+				page.should have_selector('title', :text=> 'Ruby on Rials Tutorial')
+
+	end
+end
+
+describe "Help page" do
+
+
+		it "should have the content 'Help'" do
+
+		visit '/static_pages/help'
+		page.should have_selector('h1', :text=> 'Help')
 	end
 
 
+		it "should have the right title" do
+				visit '/static_pages/home'
+				page.should have_selector('title', :text=> 'Ruby on Rials Tutorial')
+
+
+		end 
+end
+
+describe "About page" do
+
+
+		it "should have the content 'About Us'" do
+
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text=> 'About')
+
+
+		it "should have the right title" do
+				visit '/static_pages/home'
+				page.should have_selector('title', :text=> 'Ruby on Rials Tutorial')
+
+
+		end 
 	end
+end
+
+
+describe "Contact page" do
+
+
+		it "should have the content 'About Us'" do
+
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text=> 'About')
+
+
+		it "should have the right title" do
+				visit '/static_pages/contact'
+				page.should have_selector('title', :text=> 'Ruby on Rials Tutorial | Contact')
+
+
+		end 
+	end
+end
 
 
 end
